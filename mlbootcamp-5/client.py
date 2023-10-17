2737f8adad786b61a2faa09fcb89f5e887e3e0d5
Logan Wang <logan.wang@octoenergy.com>
2023-10-17 21:48:56 +1100

    hw5

---
+++ mlbootcamp-5/client.py
@@ -0,0 +1,9 @@
+import requests
+
+url = f"http://localhost:9696/predict"
+
+# client = {"job": "unknown", "duration": 270, "poutcome": "failure"}
+client = {"job": "retired", "duration": 445, "poutcome": "success"}
+
+response = requests.post(url, json=client).json()
+print(response)
